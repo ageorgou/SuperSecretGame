52055e480ddddb24b72ff063fa0db2853087b29c
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2018-06-14 19:54:40 +0100

    Prevent error if some elements not present

--- to-pdf.py
+++ to-pdf.py
@@ -9,6 +9,8 @@ import sys
 
 import scribus
 
+IMG_TYPE = 2
+
 directory = sys.argv[-1]
 filenames = glob.glob(directory + '/[0-9]*.sla')
 if not filenames:
@@ -17,8 +19,15 @@ for infile in filenames:
     print("Opening " + infile)
     scribus.openDoc(infile)
     # Auto-scale the picture
-    scribus.setScaleImageToFrame(True, name="Image9")
-    scribus.saveDoc()
+    try:
+        # Find which image object we must change
+        images = [s for s in scribus.getPageItems() if s[1] == IMG_TYPE]
+        frame = sorted(images, key=lambda s: s[2])[-1][0]
+        # frame should be "Image9" if all elements are rendered
+        scribus.setScaleImageToFrame(True, name=frame)
+        scribus.saveDoc()
+    except NoValidObjectError, err:
+        print("Image not found in " + infile)
     # Generate the PDF
     pdf = scribus.PDFfile()
     doc_name = infile.rsplit(".", 1)[0]
