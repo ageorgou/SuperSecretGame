e531b62c3b8b4a61f2df762fde2d1571aeb8cbf5
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2018-06-15 14:07:55 +0100

    Resize text in specific cards

--- to-pdf.py
+++ to-pdf.py
@@ -13,6 +13,10 @@ IMG_TYPE = 2
 
 directory = sys.argv[-1]
 filenames = glob.glob(directory + '/[0-9]*.sla')
+# Cards whose text is too long, and must therefore use a smaller font
+indices_with_long_text = [10, 12, 23, 39, 44, 46, 57]
+# HACK! I'm sorry :(
+files_with_long_text = ["{}{}.sla".format(directory, i) for i in indices_with_long_text]
 if not filenames:
     print("No documents with expected name found in " + directory)
 for infile in filenames:
@@ -28,6 +32,16 @@ for infile in filenames:
         scribus.saveDoc()
     except NoValidObjectError, err:
         print("Image not found in " + infile)
+    # Change the text size if necessary
+    print(infile)
+    if infile in files_with_long_text:
+        print("Found " + infile + "!!!")
+        try:
+            frame = "Text7"
+            scribus.setFontSize(11, frame)
+            scribus.saveDoc()
+        except NoValidObjectError, err:
+            print("Text not found in " + infile)
     # Generate the PDF
     pdf = scribus.PDFfile()
     doc_name = infile.rsplit(".", 1)[0]
